3fe60ba351dafe0ba527d88429a0608ceb438e8e
sirknight001 <109674453+sirknight001@users.noreply.github.com>
2022-09-02 13:21:52 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// document.getElementById("count").innerText = 6
+
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let count = 0
+
+
+function increment() {
+    count += 1
+    countEl.innerHTML = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    count = 0
+    countEl.innerHTML = 0
+}
+
+
